6443a8884870cd1b3f93aa38f828ada196e9d9b0
Jobot <jojo64364@gmail.com>
2021-10-05 16:29:53 +0200

    ssss

--- homeContent.php
+++ homeContent.php
@@ -3,61 +3,62 @@
 // Date : 04.10.2021
 // Descritption : Page de sommaire final
 ?>
-
-<div>
-    <div class="homeContentText">
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-            sed do eiusmod tempor incididunt ut labore et dolore magna 
-            aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
-            ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-            Duis aute irure dolor in reprehenderit in voluptate velit esse 
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat 
-            cupidatat non proident, sunt in culpa qui officia deserunt mollit 
-            anim id est laborum
-        </p>
-    </div>
-    <div class="homeContentLastOuvrages">
-        <div class="homeContentLastOuvragesTitle">
-            <h2>Derniers ouvrages ajoutés</h2>
+<div class ="homeContent">
+    <div class="homeContentContainer">
+        <div class="homeContentText">
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                sed do eiusmod tempor incididunt ut labore et dolore magna 
+                aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+                ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                Duis aute irure dolor in reprehenderit in voluptate velit esse 
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat 
+                cupidatat non proident, sunt in culpa qui officia deserunt mollit 
+                anim id est laborum
+            </p>
         </div>
-        <div class="homeContentLastOuvragesContainer">
-            <div class="lastOuvrageBox">
-                <div class="lastOuvrageBoxImg">
-                    <img src="img/bee.png" alt="bee">
-                </div>
-                <p>Cactus</p>
+        <div class="homeContentLastOuvrages">
+            <div class="homeContentLastOuvragesTitle">
+                <h2>Derniers ouvrages ajoutés</h2>
             </div>
+            <div class="homeContentLastOuvragesContainer">
+                <div class="lastOuvrageBox">
+                    <div class="lastOuvrageBoxImg">
+                        <img src="img/bee.png" alt="bee">
+                    </div>
+                    <p>Cactus</p>
+                </div>
 
-            <div class="lastOuvrageBox">
-                <div class="lastOuvrageBoxImg">
-                    <img src="img/cactus.png" alt="cactus">
+                <div class="lastOuvrageBox">
+                    <div class="lastOuvrageBoxImg">
+                        <img src="img/cactus.png" alt="cactus">
+                    </div>
+                    <p>Cactus</p>
                 </div>
-                <p>Cactus</p>
-            </div>
 
-            <div class="lastOuvrageBox">
-                <div class="lastOuvrageBoxImg">
-                    <img src="img/nuage.png" alt="nuage">
+                <div class="lastOuvrageBox">
+                    <div class="lastOuvrageBoxImg">
+                        <img src="img/nuage.png" alt="nuage">
+                    </div>
+                    <p>Cactus</p>
                 </div>
-                <p>Cactus</p>
-            </div>
-            
-            <div class="lastOuvrageBox">
-                <div class="lastOuvrageBoxImg">
-                <img src="img/pietro.png" alt="pietro">
+                
+                <div class="lastOuvrageBox">
+                    <div class="lastOuvrageBoxImg">
+                    <img src="img/pietro.png" alt="pietro">
+                    </div>
+                    <p>Cactus</p>
                 </div>
-                <p>Cactus</p>
-            </div>
-            
-            <div class="lastOuvrageBox">
-                <div class="lastOuvrageBoxImg">
-                <img src="img/pietro_grand.png" alt="pietro_grand">
+                
+                <div class="lastOuvrageBox">
+                    <div class="lastOuvrageBoxImg">
+                    <img src="img/pietro_grand.png" alt="pietro_grand">
+                    </div>
+                    <p>Cactus</p>
                 </div>
-                <p>Cactus</p>
-            </div>
+                
             
-           
+            </div>
         </div>
     </div>
 </div>
