04d5974bf132fe721c86d598ad1b702ebcd1d1a5
Jobot <jojo64364@gmail.com>
2021-10-13 21:04:13 +0200

    mise en place des required dans le form

--- ouvrageAdd.php
+++ ouvrageAdd.php
@@ -7,7 +7,6 @@ include ("model.php");
 ?>
 
 
-
 <h1>Ajout d'un ouvrage</h1>
 
 <form action="ouvrageAddCheck.php" method="post">
@@ -17,12 +16,12 @@ include ("model.php");
 
     <p>
         <label for="title">Titre</label>
-        <input type="text" name="title" />
+        <input type="text" name="title" required/>
     </p>
 
     <label for="auteur">Auteur</label>
-    <input list="auteur" name="auteur">
-    <datalist required name="auteur" id="auteur">
+    <input list="auteur" name="auteur" required>
+    <datalist name="auteur" id="auteur">
         <?php 
         for ($i=0; $i < count($t_author_names); $i++) { 
             $fullName = $t_author_names[$i][1]." ".$t_author_names[$i][0];
@@ -34,8 +33,8 @@ include ("model.php");
     <br><br>
 
     <label for="categorie">Categorie</label>
-    <input list="categorie" name="categorie">
-    <datalist required name="categorie" id="categorie">
+    <input list="categorie" name="categorie" required>
+    <datalist  name="categorie" id="categorie">
         <?php 
         for ($i=0; $i < count($t_category_name); $i++) { 
             echo "<option value=".$t_category_name[$i].">".$t_category_name[$i]."</option>";
@@ -46,8 +45,8 @@ include ("model.php");
     <br><br>
 
     <label for="editeur">Editeur</label>
-    <input list="editeur" name="editeur">
-    <datalist required name="editeur" id="editeur">
+    <input list="editeur" name="editeur" required>
+    <datalist  name="editeur" id="editeur" >
         <?php 
         for ($i=0; $i < count($t_publisher_names); $i++) { 
             echo "<option value=".$t_publisher_names[$i].">".$t_publisher_names[$i]."</option>";
@@ -67,13 +66,12 @@ include ("model.php");
 
     <p>
         <label for="pdfLink">Lien vers extrait pdf</label>
-        <input required type="url" name="pdfLink" />
+        <input  type="url" name="pdfLink" required/>
     </p>
         
     <label for="description">Descritption:</label>
     <br>
-    <textarea id="description" name="description" rows="4" cols="50">
-    </textarea>
+    <textarea id="description" name="description" rows="4" cols="50" required></textarea>
     <br>
     <input type="submit" value="Submit">
 
