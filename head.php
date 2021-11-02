d02f1df9a39335687944087cfb16898c716751a4
Nanachronisme <73257828+Nanachronisme@users.noreply.github.com>
2021-11-02 16:02:42 +0100

    bootstrap

--- head.php
+++ head.php
@@ -6,6 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="everything.css">
     <title>Ouvrage</title>
+
+        <!-- Bootstrap core CSS -->
+        <link href="resources/bootstrap-5.1.3-dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
     
