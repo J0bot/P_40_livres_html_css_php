000b510a1360fcb812e57f25a2c0514906db61bf
J0bot <jojo64364@gmail.com>
2021-11-16 17:20:51 +0100

    amélioration de la page de list

--- _final_shit/view/page/book/list.php
+++ _final_shit/view/page/book/list.php
@@ -1,4 +1,8 @@
-
+<?php 
+// Auteur : José Carlos Gasser
+// Date : 16.11.2021
+// Descritption : page de liste des livres
+?>
 <h1>Liste des ouvrages</h1>
 
 <table>
@@ -10,18 +14,16 @@
     </tr>
 
     <?php 
-        foreach ($list_books as $book) { 
-            echo "<tr>";
-            //Ici j'ai mis en 3 lignes pour éviter le code spageth
-            echo '<th><a href="index.php?page=detail&bookId='.$book["idBook"];
-            echo '"><img style="max-width:200px" src="resources/img/'.$book["booCover"];
-            echo '" alt="'.$book["booTitle"].'"></a></th>';
-            echo "<th>" . $book["booTitle"]. "</th>";
-            echo "<th>" . $book["autFirstName"]." ".$book["autLastName"]. "</th>";
-            echo "<th>" . $book["useName"]. "</th>";
-            echo "</tr>";
+        foreach ($list_books as $book) {
+            ?> 
+            <tr>
+            <th><a href="index.php?page=detail&bookId=<?=$book["idBook"];?>"><img style="max-width:200px" src="resources/img/<?=$book["booCover"];?>" alt="<?=$book["booTitle"];?>"></a></th>
+            <th><?=$book["booTitle"]?></th>
+            <th><?=$book["autFirstName"]?> <?=$book["autLastName"]?></th>
+            <th><?=$book["useName"]?></th>
+            </tr>
+            <?php
         }
     ?>
-
 </table>
 
