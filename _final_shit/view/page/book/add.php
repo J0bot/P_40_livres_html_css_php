b31fea0d78d187e091cc7031760960b107799288
pietro <78962489+kuoch00@users.noreply.github.com>
2021-12-07 14:17:44 +0100

    ajout ouvrage style

--- _final_shit/view/page/book/add.php
+++ _final_shit/view/page/book/add.php
@@ -4,80 +4,85 @@
 // Descritption : Page de ajout d'un ouvrage
 
 ?>
-
+<div class="container">
 <h1>Ajout d'un ouvrage</h1>
 
 <form action="?action=addBook" method="post" enctype="multipart/form-data">
 
+    <!-- ajout image-->
+    <div></div>
     <label for="bookImage">Importer le cover</label>
     <input type="file" id="bookImage" name="bookImage"
         accept=".jpg, .jpeg, .png" required>
 
-    <p>
-        <label for="title">Titre</label><br>
-        <input type="text" name="title" required/>
-    </p>
+    <div class="formBoxFlex">
+        <label for="title">Titre</label>
+        <input type="text" name="title" required/><br>
+    </div>
 
-    <label for="auteurNom">Nom Auteur</label> <br>
-    <input list="auteurNom" name="auteurNom" required>
-    <datalist name="auteurNom" id="auteurNom">
-        <?php 
-        foreach($authors as $author)
-        {?>
-            <option value=<?=$author["autLastName"];?>><?=$author["autLastName"]?></option> <?php   
-        }?>
-    </datalist>
+    <div class="formBoxFlex">
+        <label class="formLabelBox" for="auteurNom">Nom Auteur</label>
+        <input class="formInputBox" list="auteurNom" name="auteurNom" required> <br>
+        <datalist name="auteurNom" id="auteurNom">
+            <?php 
+            foreach($authors as $author)
+            {?>
+                <option value=<?=$author["autLastName"];?>><?=$author["autLastName"]?></option> <?php   
+            }?>
+        </datalist>
+    </div>
 
-    <br><br>
+    <div class="formBoxFlex">
+        <label for="auteurPrenom">Prénom Auteur</label> 
+        <input list="auteurPrenom" name="auteurPrenom" required><br>
+        <datalist name="auteurPrenom" id="auteurPrenom">
+            <?php 
+            foreach($authors as $author)
+            {?>
+                <option value=<?=$author["autFirstName"];?>><?=$author["autFirstName"]?></option> <?php   
+            }?>
+        </datalist>
+    </div>
 
-    <label for="auteurPrenom">Prénom Auteur</label> <br>
-    <input list="auteurPrenom" name="auteurPrenom" required>
-    <datalist name="auteurPrenom" id="auteurPrenom">
-        <?php 
-        foreach($authors as $author)
-        {?>
-            <option value=<?=$author["autFirstName"];?>><?=$author["autFirstName"]?></option> <?php   
-        }?>
-    </datalist>
+    <div class="formBoxFlex">
+        <label for="categorie">Categorie</label>
+        <input list="categorie" name="categorie" required><br>
+        <datalist  name="categorie" id="categorie">
+            <?php 
+            foreach($categories as $categorie)
+            {?>
+                <option value=<?=$categorie["catName"];?>><?=$categorie["catName"]?></option><?php   
+            }?>
+        </datalist>
+    </div>
 
-    <br><br>
+    <div class="formBoxFlex">
+        <label for="editeur">Editeur</label>
+        <input list="editeur" name="editeur" required><br>
+        <datalist  name="editeur" id="editeur" >
+            <?php    
+            foreach($publishers as $publisher)
+            {?>
+                <option value=<?=$publisher["pubName"];?>><?=$publisher["pubName"]?></option><?php   
+            }?>
+        </datalist>
+    </div>
 
-    <label for="categorie">Categorie</label><br>
-    <input list="categorie" name="categorie" required>
-    <datalist  name="categorie" id="categorie">
-        <?php 
-        foreach($categories as $categorie)
-        {?>
-            <option value=<?=$categorie["catName"];?>><?=$categorie["catName"]?></option><?php   
-        }?>
-    </datalist>
-
-    <br><br>
-
-    <label for="editeur">Editeur</label><br>
-    <input list="editeur" name="editeur" required>
-    <datalist  name="editeur" id="editeur" >
-        <?php    
-        foreach($publishers as $publisher)
-        {?>
-            <option value=<?=$publisher["pubName"];?>><?=$publisher["pubName"]?></option><?php   
-        }?>
-    </datalist>
-        <br><br>
-    <p>
+    <div class="formBoxFlex">
         <label for="yearEdition">Année d'édition</label><br>
         <input required type="number" min="1900" max="2099" step="1" value="2016" name="yearEdition" />
-    </p>
-
-    <p>
+    </div>
+    
+    <div class="formBoxFlex">
         <label for="pageNumber">Nombre de pages</label>
         <input required type="number" name="pageNumber" />
-    </p>
+    </div>
 
-    <p>
+    <div class="formBoxFlex">
         <label for="pdfLink">Lien vers extrait pdf</label>
         <input  type="url" name="pdfLink" required/>
-    </p>
+    </div>
+
         
     <label for="description">Descritption:</label>
     <br>
@@ -86,3 +91,4 @@
     <input type="submit" value="Submit">
 </form>
 
+</div>
