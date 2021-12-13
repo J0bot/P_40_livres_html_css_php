86d29de143cf4b6ff431b6508b600408e6e0bfc7
Jobot <65437977+J0bot@users.noreply.github.com>
2021-12-13 08:27:25 +0100

    petit bug

--- _final_shit/model/model.php
+++ _final_shit/model/model.php
@@ -635,7 +635,7 @@ class Database {
      */
     public function getUserReviews($idUser)
     {
-        $query = "SELECT COUNT(idReview) as nbReviews FROM t_commentary idUser=:idUser ";
+        $query = "SELECT COUNT(idReview) as nbReviews FROM t_review idUser=:idUser ";
         $binds = array (
             0 => array (
                 'var' => $idUser,
