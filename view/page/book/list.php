0f8e8f1b3a1b54961ea33c7bb9b825a481398d78
pietro <78962489+kuoch00@users.noreply.github.com>
2021-12-14 14:03:12 +0100

    cookie

--- view/page/book/list.php
+++ view/page/book/list.php
@@ -6,11 +6,10 @@
 <div class="container">
     <h1>Liste des ouvrages</h1>
 
-    <form action="?page=list" method="POST">
-        <label for="category">Afficher par catégorie :</label>
+    <form class="row" action="?page=list" method="POST">
+        <label class="col col-9 text-end align-middle catFontSize" for="category">Afficher par catégorie :</label>
 
-        <select onchange="this.form.submit()" name="category" id="category">
-
+        <select class="col col-3 form-select col col-3 noflex" onchange="this.form.submit()" name="category" id="category">
         <option value="<?=$cat?>"><?=$cat?></option>
         <?php
         if ($cat!="Tout afficher") {
@@ -44,7 +43,7 @@
 
         <?php 
             foreach ($list_books as $book) { ?>
-                <tr class="listTextStyle ">
+                <tr class="listTextStyle">
                     <th class="widthColSimple">
                         <a href="index.php?page=detail&bookId=<?=$book["idBook"]?>">
                         <img style="max-width:150px" src="resources/img/<?=$book["booCover"]?>" alt="<?=$book["booTitle"]?>"></a>
